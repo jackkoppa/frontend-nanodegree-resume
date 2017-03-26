fc30891fa34c356ccf04c6333beeb7a5e25f0797
Jack Koppa <hello@jackkoppa.com>
2017-03-26 17:04:05 -0500

    Adjust accordion functionality
    Change animation of accorion content to affect outer element, so that
    expanding is a smooth transition

--- dev/es2015/interaction.js
+++ dev/es2015/interaction.js
@@ -4,5 +4,7 @@ Functions for scripting interaction
 
 function handleAccordionClick(e) {
 	var $accordion = $(e).parent();
+	var height = $accordion.children(".accordion-content").height();
+	console.log(height);
 	$accordion.toggleClass("open");
 }
