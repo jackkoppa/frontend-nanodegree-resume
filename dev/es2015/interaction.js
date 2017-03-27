2be52b316970c2147ef9ac245d87db92c64a8869
Jack Koppa <hello@jackkoppa.com>
2017-03-26 20:50:06 -0500

    Finish change to container animation for accordions
    Set max-height for accordion container based on .accorion-content, on
    click of .accordion-header. Adjust margins and borders.

--- dev/es2015/interaction.js
+++ dev/es2015/interaction.js
@@ -4,7 +4,12 @@ Functions for scripting interaction
 
 function handleAccordionClick(e) {
 	var $accordion = $(e).parent();
-	var height = $accordion.children(".accordion-content").height();
-	console.log(height);
+	var contentHeight = $accordion.children(".accordion-content").height();
+	console.log(contentHeight);
+	if($accordion.hasClass("open")) {
+		$accordion.css("max-height","");		
+	} else {
+		$accordion.css("max-height",$accordion.height() + contentHeight);
+	}
 	$accordion.toggleClass("open");
 }
